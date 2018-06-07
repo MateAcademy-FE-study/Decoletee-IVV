ec42174d7c2000e4fa35396a22370a4397729f4f
VadymBoguslavsky <axixe92@gmail.com>
2018-06-07 20:17:40 +0300

    фтшьфешщт

--- app/js/js.js
+++ app/js/js.js
@@ -21,29 +21,49 @@ $('.play').addClass('animated bounceInDown')
 
 
 
-  $('button.form__button').click(function(e){
-    swal({
-      title: "You successfully contacted us!", 
-      text: "Thank you for your feedback", 
-      icon: "success",
-      buttons: true,
-      closeModal: false
-    });
-  })
 
-  $('.form-group .selectric-items li').click(function(e){
-    swal({
-      title: "You successfully changed language",  
-      icon: "success",
-      buttons: true,
-      closeModal: false
-    });
-  })
 
 var menuButton = document.querySelector('.menu__icon')
   menuButton.addEventListener("click", function(){
   var b = document.querySelector('body')
-if( b.style.overflow === "hidden"){
-  b.style.overflow = "visible"
-} else{b.style.overflow = "hidden"}
+  if( b.style.overflow === "hidden"){
+    b.style.overflow = "visible";
+
+  } else{b.style.overflow = "hidden"}
+})
+
+
+
+
+
+var out = {}
+var arr = document.querySelector('.form__button')
+arr.addEventListener('click', function(e){
+  e.preventDefault();
+  var data = $( 'form' ).serializeArray();
+  for(var i = 0; i < data.length; i++){
+    var record = data[i];
+    out[record.name] = record.value;
+  }
+    swal({
+      title: "You successfully contacted us!", 
+      icon: "success",
+      elem: out,
+      buttons: true,
+      closeModal: false
+    });
+});
+console.log(out)
+
+
+
+
+
+$('.form-group .selectric-items li').click(function(e){
+  swal({
+    title: "You successfully changed language",  
+    icon: "success",
+    buttons: true,
+    closeModal: false
+  });
 })
